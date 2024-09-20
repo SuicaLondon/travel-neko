5310f637c5321029c30f3c89015ee2f925968384
SuicaLondon <66697085+SuicaLondon@users.noreply.github.com>
2024-09-20 09:29:36 +0100

    feat: add sidebar config (#15)

--- travel-neko-web/src/components/sidebar/sidebar-item.tsx
+++ travel-neko-web/src/components/sidebar/sidebar-item.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from "react";
 type SidebarItemProps = {
   href: string;
   icon: ReactNode;
-  children: ReactNode;
+  children?: ReactNode;
   label?: ReactNode;
 };
 
@@ -17,11 +17,11 @@ export default function SidebarItem({
     <li>
       <a
         href={href}
-        className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group"
+        className="group flex items-center space-x-2 rounded-lg p-2 text-gray-900 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700"
       >
-        {icon}
-        <span className="ms-3">{children}</span>
-        {label}
+        {icon && <span className="text-xl">{icon}</span>}
+        {children && <span className="text-xl">{children}</span>}
+        {label && <span className="text-xl">{label}</span>}
       </a>
     </li>
   );
