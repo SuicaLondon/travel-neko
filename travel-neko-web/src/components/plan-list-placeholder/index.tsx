389fd6c45058885aa601b0e3c0cc97b254e511b6
SuicaLondon <66697085+SuicaLondon@users.noreply.github.com>
2024-09-22 15:37:12 +0200

    chore: update the placeholder of the plan list

--- travel-neko-web/src/components/plan-list-placeholder/index.tsx
+++ travel-neko-web/src/components/plan-list-placeholder/index.tsx
@@ -4,12 +4,8 @@ export default function PlanListPlaceholder() {
   return (
     <div className="flex flex-col gap-4">
       {new Array(10).fill(null).map((item, index) => (
-        <div
-          className="flex h-80 w-full max-w-96 flex-col space-y-2"
-          key={index}
-        >
-          <span className="bg-placeholder block h-10 w-full"></span>
-          <span className="bg-placeholder block w-full flex-1"></span>
+        <div className="bg-placeholder flex h-80 w-full flex-col" key={index}>
+          <span className="bg-placeholder block w-full flex-1" />
         </div>
       ))}
     </div>
