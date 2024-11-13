62a89f19d8d3dd40774f73ad2bcd027b3b848df5
SuicaLondon <66697085+SuicaLondon@users.noreply.github.com>
2024-11-13 01:02:47 +0000

    feat: add swagger support

--- travel-neko-django/travel_neko/travel_neko/urls.py
+++ travel-neko-django/travel_neko/travel_neko/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from user.views import CreateUserView
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +27,7 @@ urlpatterns = [
     path('api/user/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('api-auth/', include('rest_framework.urls') ),
     path('api/', include('plans.urls')),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
